5ef8818b82d3cf48222edf318d73826cf6f5455d
John Adrian Cruz <jhndrncrz@gmail.com>
2023-01-31 21:59:25 +0800

    on chapter 7 ex 3

---
+++ src/Ch7/Ex3.java
@@ -0,0 +1,59 @@
+package Ch7;
+
+import java.util.Arrays;
+
+public class Ex3 {
+    public static void solution() {
+        double[] testDefault = new double[10000];
+        for (int i = 0; i < 10000; i++) {
+            testDefault[i] = Math.random() * 1000;
+        }
+        double[] testSelection = testDefault.clone();
+
+        long startDefault = System.nanoTime();
+        Arrays.sort(testDefault);
+        long endDefault = System.nanoTime();
+
+        // System.out.println(Arrays.toString(testDefault));
+        System.out.printf("Arrays.sort() took %d nanoseconds.\n", endDefault - startDefault);
+
+        long startSelection = System.nanoTime();
+        selectionSort(testSelection);
+        long endSelection = System.nanoTime();
+
+        // System.out.println(Arrays.toString(testSelection));
+        System.out.printf("selectionSort() took %d nanoseconds.\n", endSelection - startSelection);
+    }
+
+    static void selectionSort(double[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            int iMax = 0;
+
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[iMax]) {
+                    iMax = j;
+                }
+            }
+
+            double temp = array[i];
+            array[i] = array[iMax];
+            array[iMax] = temp;
+        }
+    }
+
+    static void selectionSort(String[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            int iMax = 0;
+
+            for (int j = 0; j < i; j++) {
+                if (array[j].compareTo(array[iMax]) > 0) {
+                    iMax = j;
+                }
+            }
+
+            String temp = array[i];
+            array[i] = array[iMax];
+            array[iMax] = temp;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,8 @@ public class Main {
         // Ch5.Ex6.solution();
         // Ch5.Ex7.solution();
         // Ch7.Ex1.solution();
-        Ch7.Ex2.solution();
+        // Ch7.Ex2.solution();
+        Ch7.Ex3.solution();
 
     }
 }
